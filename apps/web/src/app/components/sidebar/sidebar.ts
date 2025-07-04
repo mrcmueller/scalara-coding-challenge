071b0a3ceaeeb183382072fa480c2c19a8ab29f3
Marcus Müller <mueller@kayego.de>
2025-07-04 13:00:32 +0200

    CSS setup

--- apps/web/src/app/components/sidebar/sidebar.ts
+++ apps/web/src/app/components/sidebar/sidebar.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sidebar',
   templateUrl: './sidebar.html',
+  styleUrls: ['./sidebar.scss'],
 })
 export class Sidebar {
   items = [
--- apps/web/src/app/examplePage/example.ts
+++ apps/web/src/app/examplePage/example.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'example-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './example.html',
   styleUrl: './example.scss',
 })
