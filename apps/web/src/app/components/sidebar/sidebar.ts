c2e35bb899d59c7a3d78b85c15fde6fa13c34938
Marcus Müller <mueller@kayego.de>
2025-07-07 18:29:19 +0200

    Sidebar done

--- apps/web/src/app/components/sidebar/sidebar.ts
+++ apps/web/src/app/components/sidebar/sidebar.ts
@@ -9,10 +9,28 @@ import { title } from 'process';
 export class Sidebar {
   items = [
     {
-      title: 'Immobilien',
-      icon: '',
+      title: 'Home',
+      link: '/',
+      icon: '/icons/sidebar/Logo.svg',
+      classes: 'logoIcon w-10 mb-2',
     },
-    { title: 'Kontakte', icon: '' },
-    { title: 'Beziehungen', icon: '' },
+    {
+      title: 'Immobilien',
+      link: '/immobilien',
+      icon: '/icons/sidebar/Building.svg',
+      classes: 'immobilienIcon w-6',
+    },
+    {
+      title: 'Kontakte',
+      link: '/kontakte',
+      icon: '/icons/sidebar/Contacts.svg',
+      classes: 'kontakteIcon w-6',
+    },
+    {
+      title: 'Beziehungen',
+      link: '/beziehungen',
+      icon: '/icons/sidebar/Bill.svg',
+      classes: 'beziehungenIcon w-6',
+    },
   ];
 }
