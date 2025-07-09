c5113db6ad4b28cbb5095bb2a8e14c14f5bb0a1f
Marcus Müller <mueller@kayego.de>
2025-07-10 00:25:41 +0200

    AI update to get an idea how things work

--- apps/web/src/app/components/tables/KontakteTable.ts
+++ apps/web/src/app/components/tables/KontakteTable.ts
@@ -1,29 +1,13 @@
 import { DataSource } from '@angular/cdk/table';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, switchMap, startWith } from 'rxjs';
 import { KontakteService } from '../../api/services';
 import { KontaktAntwortMitBeziehungenDto } from '../../api/models';
 import { MatTableModule } from '@angular/material/table';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+interface Delete {
+  delete: (id: string) => void;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
 
 @Component({
   selector: 'kontakte-table',
@@ -32,34 +16,40 @@ const ELEMENT_DATA: PeriodicElement[] = [
   imports: [MatTableModule],
 })
 export class KontakteTable {
-  kontakteService: KontakteService;
-  dataSource: DataSource<KontaktAntwortMitBeziehungenDto>;
+  dataSource: DataSource<KontaktAntwortMitBeziehungenDto> & Delete;
 
   constructor(kontakteService: KontakteService) {
-    this.kontakteService = kontakteService;
-    this.dataSource = new KontakteDataSource(this.kontakteService);
-  }
-
-  delete(id: string) {
-    const result = this.kontakteService.kontakteControllerLoescheKontakte({
-      id,
-    });
-
-    console.log(result.subscribe());
+    this.dataSource = new KontakteDataSource(kontakteService);
   }
 
   displayedColumns: string[] = ['name', 'adresse', 'land', 'action'];
 }
 
-export class KontakteDataSource extends DataSource<KontaktAntwortMitBeziehungenDto> {
+export class KontakteDataSource
+  extends DataSource<KontaktAntwortMitBeziehungenDto>
+  implements Delete
+{
+  kontakteService: KontakteService;
+  refresh$ = new Subject<void>();
   data: Observable<KontaktAntwortMitBeziehungenDto[]>;
 
   constructor(kontakteService: KontakteService) {
     super();
-    this.data = kontakteService.kontakteControllerKontakte();
+    this.kontakteService = kontakteService;
+    this.data = this.refresh$.pipe(
+      startWith(null),
+      switchMap(() => this.kontakteService.kontakteControllerKontakte()),
+    );
   }
 
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  delete(id: string) {
+    this.kontakteService
+      .kontakteControllerLoescheKontakte({ id })
+      .subscribe(() => {
+        this.refresh$.next();
+      });
+  }
+
   connect(): Observable<KontaktAntwortMitBeziehungenDto[]> {
     return this.data;
   }
