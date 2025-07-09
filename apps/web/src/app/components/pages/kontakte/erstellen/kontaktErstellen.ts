98b353bac9b9072450b21d0129e28d50b2218e1d
Marcus Müller <mueller@kayego.de>
2025-07-09 18:43:39 +0200

    form fields added and styled

--- apps/web/src/app/components/pages/kontakte/erstellen/kontaktErstellen.ts
+++ apps/web/src/app/components/pages/kontakte/erstellen/kontaktErstellen.ts
@@ -1,5 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import {
+  FloatLabelType,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,12 +27,30 @@ import { MatButtonModule } from '@angular/material/button';
   // imports: [RouterOutlet],
   templateUrl: './kontaktErstellen.html',
   styleUrl: './kontaktErstellen.scss',
-  imports: [MatButtonModule],
+  imports: [
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
 })
 export class KontaktErstellen {
-  protected title = 'web';
-  // private kontakteService = inject(KontakteService);
-  // kontakteSignal = toSignal(this.kontakteService.kontakteControllerKontakte(), {
-  //   initialValue: null,
-  // });
+  readonly hideRequiredControl = new FormControl(false);
+  readonly floatLabelControl = new FormControl('auto' as FloatLabelType);
+  readonly options = inject(FormBuilder).group({
+    hideRequired: this.hideRequiredControl,
+    floatLabel: this.floatLabelControl,
+  });
+  protected readonly hideRequired = toSignal(
+    this.hideRequiredControl.valueChanges,
+  );
+  protected readonly floatLabel = toSignal(
+    this.floatLabelControl.valueChanges.pipe(map((v) => v || 'auto')),
+    { initialValue: 'auto' },
+  );
 }
